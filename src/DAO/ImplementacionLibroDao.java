da301511d91596022376d3931ad572cf286f7ce3
oscar-230 <111622297+oscar-230@users.noreply.github.com>
2023-05-24 21:07:00 -0500

    Implementacion Dao

--- src/DAO/ImplementacionLibroDao.java
+++ src/DAO/ImplementacionLibroDao.java
@@ -1,6 +1,54 @@
 package DAO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import model.Libro;
 
-public class ImplementacionLibroDao {
+
+public class ImplementacionLibroDao implements LibroDao{
+    Map<Integer, Libro> LibroMap = new HashMap<>();
+
+    @Override
+    public List<Libro> getLibros() {
+        List<Libro> Libros = new ArrayList<>();
+        Iterator it = LibroMap.values().iterator();
+        while (it.hasNext()) {
+            Libros.add((Libro) it.next());
+        }
+        return Libros;
+    }
+
+    @Override
+    public Libro getLibro(int codigo, String autor) {
+        Libro libros = null;
+        if (LibroMap.containsKey(codigo)) {
+            libros = LibroMap.get(codigo);
+        }
+        return libros;
+    }
+
+    @Override
+    public boolean addLibro(Libro libros) {
+        LibroMap.put(libros.getCodigo(), libros);
+        return true;
+    }
+
+    @Override
+    public boolean updateLibro(int codigo, Libro libros) {
+        String genero ="";
+        String titulo = "";
+        String autor = "";
+       
+        return true;
+    }
+
+    @Override
+    public boolean deleteLibro(int codigo, Libro libros) {
+        LibroMap.remove(libros.getCodigo());
+        return true;
+    }
     
 }
